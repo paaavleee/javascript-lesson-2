6ccc0880209e1a55c08b11a16503e676455f72e6
paaavleee <kurtanidzepavle@gmail.com>
2022-10-27 16:09:26 +0400

    fin

---
+++ app.js
@@ -0,0 +1,50 @@
+const arrayNumber = [5,8,10,3,7];
+
+console.log(arrayNumber)
+
+console.log(arrayNumber.length)
+console.log(arrayNumber[2])
+
+const math = arrayNumber.length
+console.log(math)
+
+const arithmeticAverage = arrayNumber [0] + arrayNumber [1] + arrayNumber [2] + arrayNumber [3] + arrayNumber [4] ;
+
+console.log(arithmeticAverage)
+
+let division = arithmeticAverage / math;
+console.log(division)
+
+// meore davaleba
+
+const pupil = [
+    {
+        name : "demere",
+        age : 7,
+    }
+];
+
+console.log(pupil)
+
+// მესამე დავალება
+
+let info = {
+    firsName : "pavle",
+    lastName : "kurtanidze",
+    age : 33,
+    phoneNumber : 597127171,
+    address : "wereteli",
+    zipCode : "0102",
+};
+
+console.log(info)
+
+console.log(info.firsName, info.age)
+
+// 4 davaleba
+
+const myInfo =`User name is ${info.firsName}. user age is ${info.age}.user address is ${info.address}.`
+
+console.log(myInfo)
+
+
